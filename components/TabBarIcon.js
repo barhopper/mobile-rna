c5f154e9d6fb287eda872757f0a24c99724cbd76
Josh <Kersting.Josh@Gmail.com>
2020-04-06 17:22:33 -0400

    testing change to .lintstagedrd

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,15 +1,15 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as React from 'react';
+import {Ionicons} from '@expo/vector-icons'
+import * as React from 'react'
 
-import Colors from '../constants/Colors';
+import Colors from '../constants/Colors'
 
 export default function TabBarIcon(props) {
   return (
     <Ionicons
-      name={`${Platform.OS === "ios" ? "ios" : "md"}-${props.name}`}
+      name={`${Platform.OS === 'ios' ? 'ios' : 'md'}-${props.name}`}
       size={30}
-      style={{ marginBottom: -3 }}
+      style={{marginBottom: -3}}
       color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
     />
-  );
+  )
 }
