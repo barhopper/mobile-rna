388ecfd42d2491379981f376eff994b7c26d70bc
Josh <Kersting.Josh@Gmail.com>
2020-06-21 14:56:24 -0400

    fixed bug where rating wasnt what was selected

--- screens/HomeStack/BarReviewScreen.js
+++ screens/HomeStack/BarReviewScreen.js
@@ -14,7 +14,7 @@ import {getQuestions, submitReview} from '../../actions/bars'
 
 import {default as theme} from '../../constants/Theme'
 
-const outOf10 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+const outOf10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 export default function BarReviewScreen({navigation, route}) {
   const [formData, setFormData] = useState({})
