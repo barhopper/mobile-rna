db72e17f2acd53683faca74b9a988d02bc8bd86e
Jake Cadle <jcadle90@gmail.com>
2020-10-16 03:00:21 -0400

    Update promotions.js

--- actions/promotions.js
+++ actions/promotions.js
@@ -37,7 +37,6 @@ export function searchForPromotions(_keys, distance, position, options = {}) {
   const promotions = []
   barsRef.get().then(snapshot => {
     promotionRef
-      .doc()
       .doc(snapshot.data().promoId)
       .where('timeslot', '==', timeslot.toDate())
       .get()
